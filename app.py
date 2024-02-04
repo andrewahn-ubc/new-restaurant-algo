7d9888ae6dd64b9747355d08d269cf1c95eb5274
parkrafael <parkrafael11@gmail.com>
2024-02-04 14:46:00 -0800

    table_setup finished

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
 import pandas as pd
 import numpy as np
 import sqlite3
+import re
 
-con = sqlite3.connect("test_data.db")
+def table_setup(user_id_1, user_id_2 = None, user_id_3 = None, user_id_4 = None):
+    # change according to database
+    con = sqlite3.connect("test_data.db")
 
-df = pd.read_sql_query("SELECT * from restaurant", con)
+    # selecting the rows by user
+    df = pd.read_sql_query(
+        'SELECT * '
+        'FROM restaurant r '
+        f' WHERE user_id = "{user_id_1}" '
 
-# Use pivot_table instead of pivot
-pivot_df = df.pivot_table(
-    index=['user_id'],
-    columns=['restaurant'],
-    values=['rating']
-)
+        'UNION '
 
-# Save the HTML representation to a file
-pivot_df.to_html("output.html")
+        'SELECT * '
+        'FROM restaurant r2 '
+        f' WHERE user_id = "{user_id_2}"',
+        con,
+        )
+    
+    # creating a pivot table
+    pivot_df = df.pivot_table(
+        index=['user_id'],
+        columns=['restaurant'],
+        values=['rating']
+    ).fillna(0)
 
-con.close()
+    # saving the pivot table to a html file -- TESTING
+    pivot_df.to_html("output.html")
+
+    con.close()
+
+table_setup('A', 'B')
